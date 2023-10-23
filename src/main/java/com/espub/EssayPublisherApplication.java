a096947b20304a326a88f97b7bb451668954390c
Alexey Letunov <xnihonsamuraix@gmail.com>
2023-10-24 00:11:36 +0300

    init

---
+++ src/main/java/com/espub/EssayPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.espub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssayPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssayPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espub/EssayPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssayPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
